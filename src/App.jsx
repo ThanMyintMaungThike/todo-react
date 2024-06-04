b47999f8f03b6d62e63ed6742f825be19db9280c
Than Myint Maung Thike <thanmyintmaungthike@aceplussolutions.com>
2024-06-04 23:18:50 +0630

    item toggle add del

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import React, { useRef, useState } from "react";
+import { Item } from "./Item";
+
+export const App = () => {
+  const inputRef = useRef();
+
+  const [data, setData] = useState([
+    { id: 1, name: "Apple", done: true },
+    { id: 2, name: "Banana", done: false },
+    { id: 3, name: "Orange", done: false },
+  ]);
+
+  const add = (name) => {
+    const id = data[data.length - 1].id + 1;
+    setData([...data, { id, name, done: false }]);
+  };
+
+  const remove = (id) => {
+    setData(data.filter((data) => data.id !== id));
+  };
+
+  const toggle = (id) => {
+    setData(
+      data.map((item) => {
+        if (item.id === id) item.done = !item.done;
+        return item;
+      })
+    );
+  };
+
+  return (
+    <div>
+      <h1>Todo List ({data.filter((item) => !item.done).length})</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+
+          const name = inputRef.current.value;
+          if (!name) return false;
+          add(name);
+
+          inputRef.current.value = "";
+          inputRef.current.focus();
+        }}>
+        <input type="text" ref={inputRef} />
+        <button>Add</button>
+      </form>
+      <ul>
+        {data
+          .filter((item) => !item.done)
+          .map((item) => {
+            return (
+              <Item key={item.id} item={item} remove={remove} toggle={toggle} />
+            );
+          })}
+      </ul>
+
+      <hr />
+
+      <ul>
+        {data
+          .filter((item) => item.done)
+          .map((item) => {
+            return (
+              <Item key={item.id} item={item} remove={remove} toggle={toggle} />
+            );
+          })}
+      </ul>
+    </div>
+  );
+};
---
+++ src/Item.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Item = ({ item, remove, toggle }) => {
+  // const name = props;
+  return (
+    <div>
+      <li>
+        {item.done ? (
+          <button onClick={() => toggle(item.id)}>Undo</button>
+        ) : (
+          <button onClick={() => toggle(item.id)}>Done</button>
+        )}
+        {item.name}
+        <button onClick={() => remove(item.id)}>Del</button>
+      </li>
+    </div>
+  );
+};
