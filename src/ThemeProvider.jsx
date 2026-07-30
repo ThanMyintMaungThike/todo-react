29df8ddb84cb22ed118d569c39d9fc83d1f9da93
ThanMyintMaungThike <thanmyintmaungthike@aceplussolutions.com>
2026-07-30 14:18:30 +0630

    dark and light mode with theme provider

---
+++ src/ThemeProvider.jsx
@@ -0,0 +1,13 @@
+import { createContext, useState } from "react";
+import App from "./App";
+
+export const ThemeContext = createContext();
+
+export default function ThemeProvider() {
+  const [mode, setMode] = useState("light");
+  return (
+    <ThemeContext.Provider value={{ mode, setMode }}>
+      <App />
+    </ThemeContext.Provider>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App";
-import "./index.css";
+import App from "./App";
+import ThemeProvider from "./ThemeProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ThemeProvider />
+  </React.StrictMode>,
 );
