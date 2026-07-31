c832a49cebab579c32f7578cf65af0519f0d2921
ThanMyintMaungThike <thanmyintmaungthike@aceplussolutions.com>
2026-07-31 09:30:29 +0630

    Revert "dark and light mode with theme provider"
    This reverts commit 29df8ddb84cb22ed118d569c39d9fc83d1f9da93.

--- src/App.jsx
+++ src/App.jsx
@@ -1,91 +1,72 @@
-import { useState } from "react";
-import Home from "./Home";
-import { useContext } from "react";
-import { ThemeContext } from "./ThemeProvider";
-export default function App() {
-  // let mode = "dark";
-  const { mode } = useContext(ThemeContext);
+import React, { useRef, useState } from "react";
+import { Item } from "./Item";
+
+export const App = () => {
+  const inputRef = useRef();
+
+  const [data, setData] = useState([
+    { id: 1, name: "Apple", price: "40", done: true },
+    { id: 2, name: "Banana", price: "25", done: false },
+    { id: 3, name: "Orange", price: "15", done: false },
+  ]);
+
+  const add = (name) => {
+    const id = data[data.length - 1].id + 1;
+    setData([...data, { id, name, done: false }]);
+  };
+
+  const remove = (id) => {
+    setData(data.filter((data) => data.id !== id));
+  };
+
+  const toggle = (id) => {
+    setData(
+      data.map((item) => {
+        if (item.id === id) item.done = !item.done;
+        return item;
+      })
+    );
+  };
 
   return (
-    <div
-      style={{
-        background: mode === "light" ? "white" : "black",
-        color: mode === "light" ? "black" : "white",
-        padding: 20,
-      }}
-    >
-      <Home />
+    <div>
+      <h1>Todo List ({data.filter((item) => !item.done).length})</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+
+          const name = inputRef.current.value;
+          if (!name) return false;
+          add(name);
+
+          inputRef.current.value = "";
+          inputRef.current.focus();
+        }}>
+        <input type="text" ref={inputRef} />
+        <button>Add</button>
+      </form>
+
+      <ul>
+        {data
+          .filter((item) => !item.done) 
+          .map((item) => {
+            return (
+              <Item key={item.id} item={item} remove={remove} toggle={toggle} />
+            );
+          })}
+      </ul>
+
+      <hr />  
+
+      <ul>
+        {data
+          .filter((item) => item.done)
+          .map((item) => {
+            return (
+              <Item key={item.id} item={item} remove={remove} toggle={toggle} />
+            );
+          })}
+      </ul>
     </div>
   );
-}
-
-// import React, { useRef, useState } from "react";
-// import { Item } from "./Item";
-
-// export const App = () => {
-//   const inputRef = useRef();
-
-//   const [data, setData] = useState([
-//     { id: 1, name: "Apple", price: "40", done: true },
-//     { id: 2, name: "Banana", price: "25", done: false },
-//     { id: 3, name: "Orange", price: "15", done: false },
-//   ]);
-
-//   const add = (name) => {
-//     const id = data[data.length - 1].id + 1;
-//     setData([...data, { id, name, done: false }]);
-//   };
-
-//   const remove = (id) => {
-//     setData(data.filter((data) => data.id !== id));
-//   };
-
-//   const toggle = (id) => {
-//     setData(
-//       data.map((item) => {
-//         if (item.id === id) item.done = !item.done;
-//         return item;
-//       })
-//     );
-//   };
-
-//   return (
-//     <div>
-//       <h1>Todo List ({data.filter((item) => !item.done).length})</h1>
-//       <form
-//         onSubmit={(e) => {
-//           e.preventDefault();
-
-//           const name = inputRef.current.value;
-//           if (!name) return false;
-//           add(name);
-
-//           inputRef.current.value = "";
-//           inputRef.current.focus();
-//         }}
-//       >
-//         <input type="text" ref={inputRef} />
-//         <button>Add</button>
-//       </form>
-//       <ul>
-//         {data
-//           .filter((item) => !item.done)
-//           .map((item) => {
-//             return (
-//               <Item key={item.id} item={item} remove={remove} toggle={toggle} />
-//             );
-//           })}
-//       </ul>
-//       <hr /> <hr />
-//       <ul>
-//         {data
-//           .filter((item) => item.done)
-//           .map((item) => {
-//             return (
-//               <Item key={item.id} item={item} remove={remove} toggle={toggle} />
-//             );
-//           })}
-//       </ul>
-//     </div>
-//   );
-// };
+};
